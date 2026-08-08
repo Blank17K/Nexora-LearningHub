c78ec9b50735dbb86360bad76cfaae9371a983e0
Blank17K <tshwaleboikanyo25@gmail.com>
2026-08-08 18:04:18 +0200

    addProgrameFiles

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+
+app.get("/", (req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server Listening on ${PORT}` );
+})
+
+
